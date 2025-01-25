7333151e7e1bea613a55e31ce5613db1935fc5f0
Kegan Maher <kegan@compiler.la>
2025-01-24 17:13:13 -0800

    chore(terraform): simplify init
    there's only 1 subscription

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 set -e
-
 
 ENV=$1
 
@@ -11,17 +9,18 @@ if [ $# -ne 1 ]; then
 fi
 
 printf "Intializing Terraform...\n\n"
+
 # automatically inject the subscription ID
 PROD_ID=$(az account list --query "[?name == 'CDT/ODI Production'] | [0].id" --output tsv)
 terraform init -backend-config="subscription_id=$PROD_ID"
 
-printf "\n\nSelecting the Terraform workspace...\n"
+# there's only a single subscription being used
+SUBSCRIPTION="CDT/ODI Production"
+
 if [ "$ENV" = "prod" ]; then
   terraform workspace select default
-  SUBSCRIPTION="CDT/ODI Production"
 else
   terraform workspace select "$ENV"
-  SUBSCRIPTION="CDT/ODI Development"
 fi
 
 echo "Setting the subscription for the Azure CLI..."
