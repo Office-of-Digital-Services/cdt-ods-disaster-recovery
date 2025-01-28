c7624445e6cf5041083b05299fa7f8ce9655aa7e
Kegan Maher <kegan@compiler.la>
2025-01-28 09:16:07 +0000

    refactor(session): dynamic props for get/set

---
+++ web/vital_records/session.py
@@ -0,0 +1,20 @@
+from web.core.models.userflow import UserFlow
+from web.core.session import Session as CoreSession
+
+
+def userflow() -> UserFlow | None:
+    return UserFlow.objects.filter(system_name="vital-records").first()
+
+
+class Session(CoreSession):
+    def __init__(self, request, reset=False):
+        super().__init__(request, reset)
+        if reset:
+            self.userflow = userflow()
+            if self.userflow:
+                self.oauth_config = self.userflow.oauth_config
+                self.oauth_scopes = self.userflow.scopes
+                self.oauth_claims_check = self.userflow.all_claims
+                self.oauth_claims_eligibility = self.userflow.eligibility_claim
+                self.oauth_redirect_failure = self.userflow.redirect_failure
+                self.oauth_redirect_success = self.userflow.redirect_success
