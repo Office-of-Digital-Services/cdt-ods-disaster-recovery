4d96701239d9578b98c12c9ebaeea188f805e03a
Kegan Maher <kegan@compiler.la>
2025-03-03 23:30:43 +0000

    feat: install cdt_identity app

--- web/settings.py
+++ web/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cdt_identity",
     "web.core",
     "web.oauth",
     "web.vital_records",
