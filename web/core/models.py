3de94ddc9dcc5c7e85b7b2243aaf59fbf48721a8
Kegan Maher <kegan@compiler.la>
2025-03-05 00:40:31 +0000

    refactor(urls): register the flow explicitly
    remove all the complicated dynamic registration
    
    no need for label field

--- web/core/models.py
+++ web/core/models.py
@@ -1,4 +1,3 @@
-import importlib
 from uuid import uuid4
 
 from django.db import models
@@ -14,17 +13,9 @@ class UserFlow(models.Model):
         default=uuid4,
         editable=False,
     )
-    label = models.CharField(
-        help_text="A human readable label, used as the display text (user-facing)",
-        max_length=50,
-    )
     system_name = models.SlugField(
-        help_text="Internal system name for this flow, mapped to the root URL.",
+        help_text="Internal system name for this flow.",
         unique=True,
-    )
-    urlconf_path = models.CharField(
-        help_text="Django app path to the URLconf for this flow.",
-        max_length=100,
     )
     oauth_config = models.ForeignKey(
         IdentityGatewayConfig,
@@ -36,26 +27,3 @@ class UserFlow(models.Model):
         on_delete=models.PROTECT,
         help_text="The claims request details for this flow.",
     )
-
-    @property
-    def index_url(self):
-        try:
-            match = [url for url in self.urlpatterns if url.pattern.regex.match("")]
-            index = match[0]
-            return f"{self.urlconf.app_name}:{index.name}"
-        except Exception:
-            return None
-
-    @property
-    def urlconf(self):
-        try:
-            return importlib.import_module(self.urlconf_path)
-        except Exception:
-            return None
-
-    @property
-    def urlpatterns(self):
-        try:
-            return self.urlconf.urlpatterns
-        except Exception:
-            return []
--- web/wsgi.py
+++ web/wsgi.py
@@ -9,18 +9,8 @@ import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.urls import include, path
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 logger = logging.getLogger(__name__)
 application = get_wsgi_application()
-
-try:
-    import web.urls
-    from web.core.models import UserFlow
-
-    for flow in UserFlow.objects.all():
-        web.urls.urlpatterns.append(path(f"{flow.system_name}/", include(flow.urlconf_path)))
-except Exception as ex:
-    logger.error(ex)
