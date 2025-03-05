c94bf7d4500532627374a4946ba8e97cb05f35a6
Kegan Maher <kegan@compiler.la>
2025-03-05 01:03:19 +0000

    feat(hooks): vital_records app hooks

---
+++ web/vital_records/hooks.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect
+
+from web.core.hooks import DisasterRecoveryHooks
+
+
+class VitalRecordsHooks(DisasterRecoveryHooks):
+    @classmethod
+    def cancel_login(cls, request):
+        super().cancel_login(request)
+        return redirect("vital_records:unverified")
+
+    @classmethod
+    def claims_verified_eligible(cls, request, claims_request, claims_result):
+        super().claims_verified_eligible(request, claims_request, claims_result)
+        return redirect("vital_records:request")
+
+    @classmethod
+    def claims_verified_not_eligible(cls, request, claims_request, claims_result):
+        super().claims_verified_not_eligible(request, claims_request, claims_result)
+        return redirect("vital_records:unverified")
