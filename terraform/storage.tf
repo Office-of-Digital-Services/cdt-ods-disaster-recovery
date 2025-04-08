b31eedec985a5d6832bab39c8262662e62078624
Kegan Maher <kegan@compiler.la>
2025-04-07 19:44:45 -0700

    chore(terraform): update storage setting
    showing as a change true -> false in plan output
    
    must be a new default in 4.x

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,9 +1,10 @@
 resource "azurerm_storage_account" "main" {
-  name                     = "sacdtddrc${lower(local.env_letter)}001"
-  location                 = data.azurerm_resource_group.main.location
-  resource_group_name      = data.azurerm_resource_group.main.name
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
+  name                             = "sacdtddrc${lower(local.env_letter)}001"
+  location                         = data.azurerm_resource_group.main.location
+  resource_group_name              = data.azurerm_resource_group.main.name
+  account_tier                     = "Standard"
+  account_replication_type         = "RAGRS"
+  cross_tenant_replication_enabled = false
 
   blob_properties {
     last_access_time_enabled = true
