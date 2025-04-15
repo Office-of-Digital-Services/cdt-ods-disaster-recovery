e367619657d5d23421d69a38ecc56b1c9289f5f7
Kegan Maher <kegan@compiler.la>
2025-04-15 22:36:31 +0000

    feat(commands): reset_db reinitializes Django database
    - connects to POSTGRES_HOST as an elevated POSTGRES_USER
    - drops and recreates DJANGO_USER
    - drops and recreates DJANGO_DB, giving ownership to DJANGO_USER
      with some other minor configuration

--- bin/reset_db.sh
+++ bin/reset_db.sh
@@ -5,12 +5,7 @@ set -ex
 DB_RESET="${DJANGO_DB_RESET:-true}"
 
 if [[ $DB_RESET = true ]]; then
-    # construct the path to the database file from environment or default
-    DB_DIR="${DJANGO_STORAGE_DIR:-.}"
-    DB_FILE="${DJANGO_DB_FILE:-django.db}"
-    DB_PATH="${DB_DIR}/${DB_FILE}"
-
-    rm -f "${DB_PATH}"
+    python manage.py reset_db
 
     # run database migrations and other initialization
     bin/init.sh
