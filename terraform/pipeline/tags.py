82c0546f005e28666311f553ad2aa610008d61f5
Kegan Maher <kegan@compiler.la>
2025-04-15 20:18:14 -0700

    fix(terraform): normalize container_tag variable
    - always use the commit SHA
    - rename for consistency

--- terraform/pipeline/tags.py
+++ terraform/pipeline/tags.py
@@ -10,24 +10,19 @@ SOURCE_BRANCH = os.environ["SOURCE_BRANCH"]
 IS_TAG = SOURCE_BRANCH.startswith("refs/tags/") is True
 
 tag_type = None
-image_tag = COMMIT_SHA
 
 if REASON == "IndividualCI" and IS_TAG:
     if re.fullmatch(r"20\d\d.\d\d.\d+-rc\d+", SOURCE):
         tag_type = "test"
     elif re.fullmatch(r"20\d\d.\d\d.\d+", SOURCE):
         tag_type = "prod"
-else:
-    # we haven't deployed a new image tag yet, use the main branch
-    image_tag = "main"
 
 print(f"REASON: {REASON}")
 print(f"INDIVIDUAL_SOURCE: {SOURCE}")
 print(f"SOURCE_BRANCH: {SOURCE_BRANCH}")
 print(f"COMMIT_SHA: {COMMIT_SHA}")
-print(f"image_tag: {image_tag}")
 print(f"tag_type: {tag_type}")
 
 # https://learn.microsoft.com/en-us/azure/devops/pipelines/process/set-variables-scripts?view=azure-devops&tabs=bash#about-tasksetvariable
-print(f"##vso[task.setvariable variable=image_tag]{image_tag}")
+print(f"##vso[task.setvariable variable=container_tag]{COMMIT_SHA}")
 print(f"##vso[task.setvariable variable=tag_type]{tag_type}")
