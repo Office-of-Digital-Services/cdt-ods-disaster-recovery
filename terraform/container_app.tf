a67b2a2f4cf2490ef311835a766b3c4efe699bca
Kegan Maher <kegan@compiler.la>
2025-04-16 01:51:09 +0000

    feat(terraform): define db container app
    make web container app depend on this container app
    and get the POSTGRES_HOSTNAME env var from this app's FQDN

--- terraform/container_app.tf
+++ terraform/container_app.tf
@@ -9,6 +9,102 @@ resource "azurerm_container_app_environment" "main" {
   }
 }
 
+resource "azurerm_container_app" "db" {
+  name                         = lower("aca-cdt-pub-vip-ddrc-${local.env_letter}-db")
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = data.azurerm_resource_group.main.name
+  revision_mode                = "Single"
+  max_inactive_revisions       = 10
+  workload_profile_name        = "Consumption"
+
+  identity {
+    identity_ids = []
+    type         = "SystemAssigned"
+  }
+
+  secret {
+    name                = "postgres-db"
+    key_vault_secret_id = "${local.secret_http_prefix}/postgres-db"
+    identity            = "System"
+  }
+  secret {
+    name                = "postgres-user"
+    key_vault_secret_id = "${local.secret_http_prefix}/postgres-user"
+    identity            = "System"
+  }
+  secret {
+    name                = "postgres-password"
+    key_vault_secret_id = "${local.secret_http_prefix}/postgres-password"
+    identity            = "System"
+  }
+
+  # internal only, TCP port 5432
+  ingress {
+    external_enabled = false
+    target_port      = 5432
+    transport        = "tcp"
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  template {
+    min_replicas = 1
+    max_replicas = 1
+
+    # define the persistent volume using Azure File Share
+    volume {
+      name         = "pgdata-volume"
+      storage_type = "AzureFile"
+      storage_name = azurerm_storage_share.postgres.name
+    }
+
+    container {
+      name   = "postgres"
+      image  = "postgres:17"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      # Environment variables using secrets for PostgreSQL initialization
+      env {
+        name        = "POSTGRES_DB"
+        secret_name = "postgres-db"
+      }
+      env {
+        name        = "POSTGRES_USER"
+        secret_name = "postgres-user"
+      }
+      env {
+        name        = "POSTGRES_PASSWORD"
+        secret_name = "postgres-password"
+      }
+      env {
+        name = "PGDATA"
+        # Standard location within the volume mount
+        value = "/var/lib/postgresql/data/pgdata"
+      }
+
+      # Mount the persistent volume
+      volume_mounts {
+        # Must match the volume name defined above
+        name = "pgdata-volume"
+        # Standard PostgreSQL data directory
+        path = "/var/lib/postgresql/data"
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  depends_on = [
+    azurerm_storage_account.main,
+    azurerm_storage_share.postgres
+  ]
+}
+
 resource "azurerm_container_app" "web" {
   name                         = lower("aca-cdt-pub-vip-ddrc-${local.env_letter}-web") # has to be lowercase
   container_app_environment_id = azurerm_container_app_environment.main.id
@@ -27,8 +123,18 @@ resource "azurerm_container_app" "web" {
     identity            = "System"
   }
   secret {
-    name                = "django-storage-dir"
-    key_vault_secret_id = "${local.secret_http_prefix}/django-storage-dir"
+    name                = "django-db-name"
+    key_vault_secret_id = "${local.secret_http_prefix}/django-db-name"
+    identity            = "System"
+  }
+  secret {
+    name                = "django-db-user"
+    key_vault_secret_id = "${local.secret_http_prefix}/django-db-user"
+    identity            = "System"
+  }
+  secret {
+    name                = "django-db-password"
+    key_vault_secret_id = "${local.secret_http_prefix}/django-db-password"
     identity            = "System"
   }
   secret {
@@ -52,12 +158,12 @@ resource "azurerm_container_app" "web" {
     identity            = "System"
   }
 
+  # external, auto port 8000
   ingress {
     client_certificate_mode = "ignore"
     external_enabled        = true
-
-    target_port = 8000
-    transport   = "auto"
+    target_port             = 8000
+    transport               = "auto"
     traffic_weight {
       percentage      = 100
       latest_revision = true
@@ -95,8 +201,21 @@ resource "azurerm_container_app" "web" {
         secret_name = "django-allowed-hosts"
       }
       env {
-        name        = "DJANGO_STORAGE_DIR"
-        secret_name = "django-storage-dir"
+        name        = "DJANGO_DB_NAME"
+        secret_name = "django-db-name"
+      }
+      env {
+        name        = "DJANGO_DB_USER"
+        secret_name = "django-db-user"
+      }
+      env {
+        name        = "DJANGO_DB_PASSWORD"
+        secret_name = "django-db-password"
+      }
+      env {
+        name = "POSTGRES_HOSTNAME"
+        # Use the FQDN of the internal database container app
+        value = azurerm_container_app.db.latest_revision_fqdn
       }
       env {
         name        = "DJANGO_DEBUG"
@@ -121,4 +240,8 @@ resource "azurerm_container_app" "web" {
   lifecycle {
     ignore_changes = [tags]
   }
+
+  depends_on = [
+    azurerm_container_app.db
+  ]
 }
