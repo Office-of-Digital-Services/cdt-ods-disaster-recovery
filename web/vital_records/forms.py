ddfcad7b47a44a78f08d8aabde7831eb46d7f3a3
Machiko Yasuda <machiko@compiler.la>
2025-04-17 17:45:12 +0000

    feat: style the first form

--- web/vital_records/forms.py
+++ web/vital_records/forms.py
@@ -3,7 +3,11 @@ from web.core.models import VitalRecordsRequest
 
 
 class RequestEligibilityForm(forms.ModelForm):
-    fire = forms.ChoiceField(choices=VitalRecordsRequest.FIRE_CHOICES, widget=forms.Select(attrs={"class": "form-control"}))
+    fire = forms.ChoiceField(
+        choices=VitalRecordsRequest.FIRE_CHOICES,
+        label="Please confirm the fire you were impacted by",
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
 
     class Meta:
         model = VitalRecordsRequest
