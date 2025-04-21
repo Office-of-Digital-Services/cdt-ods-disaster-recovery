61d9bb3254de48e0cdf65090dc9c35a8112b0b33
Machiko Yasuda <machiko@compiler.la>
2025-04-21 21:14:46 +0000

    feat(eligibility): add url, form, view, template for first form

--- web/vital_records/views.py
+++ web/vital_records/views.py
@@ -2,10 +2,14 @@ from typing import Any
 
 from django.http import HttpRequest
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 
+from web.vital_records.models import VitalRecordsRequest
 from web.vital_records.session import Session
+from web.vital_records.forms import EligibilityForm
 
 
 class IndexView(TemplateView):
@@ -30,6 +34,24 @@ class RequestView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["email"] = session.verified_email
         return context
+
+
+class EligibilityView(CreateView):
+    model = VitalRecordsRequest
+    form_class = EligibilityForm
+    template_name = "vital_records/request/eligibility.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Move form state to next state
+        self.object.complete_eligibility()
+        self.object.save()
+
+        return response
+
+    def get_success_url(self):
+        return reverse("vital_records:submitted")
 
 
 class SubmittedView(TemplateView):
