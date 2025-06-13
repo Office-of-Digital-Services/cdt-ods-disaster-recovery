bb6e69007c138230b53ffef2a703487e14de284a
Kegan Maher <kegan@compiler.la>
2025-06-13 11:58:29 -0700

    feat(terraform): worker uses email services
    - generate secrets for the connection string and email from address
    - reference secrets in worker container app

--- terraform/email.tf
+++ terraform/email.tf
@@ -1,5 +1,7 @@
 locals {
   azure_communication_name                   = "ACS-PUB-VIP-DDRC-${local.env_letter}-001"
+  azure_communication_connection_string_name = "azure-communication-connection-string"
+  azure_communication_from_email_name        = "azure-communication-from-email"
 }
 
 resource "azurerm_communication_service" "main" {
@@ -39,4 +41,25 @@ resource "azurerm_email_communication_service_domain" "azure_managed" {
 resource "azurerm_communication_service_email_domain_association" "azure_managed" {
   communication_service_id = azurerm_communication_service.main.id
   email_service_domain_id  = azurerm_email_communication_service_domain.azure_managed.id
+}
+
+resource "azurerm_key_vault_secret" "azure_communication_connection_string" {
+  name         = local.azure_communication_connection_string_name
+  value        = azurerm_communication_service.main.primary_connection_string
+  key_vault_id = azurerm_key_vault.main.id
+  content_type = "password"
+  depends_on = [
+    azurerm_key_vault.main,
+    azurerm_email_communication_service.main # Ensure Communication Service exists
+  ]
+}
+
+resource "azurerm_key_vault_secret" "azure_communication_from_email" {
+  name         = local.azure_communication_from_email_name
+  value        = "DoNotReply@${azurerm_email_communication_service_domain.azure_managed.mail_from_sender_domain}"
+  key_vault_id = azurerm_key_vault.main.id
+  depends_on = [
+    azurerm_key_vault.main,
+    azurerm_email_communication_service_domain.azure_managed # Ensure domain exists
+  ]
 }
