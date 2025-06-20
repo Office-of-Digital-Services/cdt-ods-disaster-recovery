92d684703cc8a2579127f5703c7d63791245a362
Kegan Maher <kegan@compiler.la>
2025-06-19 23:18:54 -0700

    feat(tf/network): define vnet and public subnet
    each environment will have a vnet defined
    
    the public subnet will be for Azure Container Apps that need
    to be available on the public Internet

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,9 +1,32 @@
 locals {
-  # VNet uses a shared Resource Group, different from App Service Resource Group we use for DDRC stuff
-  network_resource_group_name = "RG-CDT-PUB-SHRD-W-P-001"
-  vnet_name                   = "VNET-CDT-PUB-SHRD-W-P-001"
-  subnet_name                 = "SNET-CDT-PUB-DDRC-${local.env_letter}-001"
+  vnet_name          = "VNET-CDT-PUB-VIP-DDRC-${local.env_letter}-001"
+  subnet_name_prefix = "SNET-CDT-PUB-VIP-DDRC-${local.env_letter}"
+}
 
-  # use the subnet ID rather than referencing it as a data source to work around permissions issues
-  subnet_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.network_resource_group_name}/providers/Microsoft.Network/virtualNetworks/${local.vnet_name}/subnets/${local.subnet_name}"
+# The primary VNet (per environment)
+resource "azurerm_virtual_network" "main" {
+  name                = local.vnet_name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# The primary subnet for public (Internet) access
+resource "azurerm_subnet" "public" {
+  name                 = "${local.subnet_name_prefix}-public"
+  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "Microsoft.App/environments"
+    service_delegation {
+      name    = "Microsoft.App/environments"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+  default_outbound_access_enabled = false
 }
