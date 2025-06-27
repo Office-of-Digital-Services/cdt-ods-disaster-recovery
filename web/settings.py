6f6c3fe640a605c1b3b91efab860a6e58e97378e
Kegan Maher <kegan@compiler.la>
2025-06-27 09:22:00 -0700

    fix(settings): correct environment detection
    update hosts

--- web/settings.py
+++ web/settings.py
@@ -42,11 +42,11 @@ def RUNTIME_ENVIRONMENT():
     # usage of django.conf.settings.ALLOWED_HOSTS here (rather than the module variable directly)
     # is to ensure dynamic calculation, e.g. for unit tests and elsewhere this setting is needed
     env = RUNTIME_ENVS.LOCAL
-    if any(["dev" in host for host in settings.ALLOWED_HOSTS]):
+    if any(["cdt-pub-vip-ddrc-d" in host for host in settings.ALLOWED_HOSTS]):
         env = RUNTIME_ENVS.DEV
-    elif any(["test" in host for host in settings.ALLOWED_HOSTS]):
+    elif any(["cdt-pub-vip-ddrc-t" in host for host in settings.ALLOWED_HOSTS]):
         env = RUNTIME_ENVS.TEST
-    elif "disasterrecovery.ca.gov" in settings.ALLOWED_HOSTS:
+    elif any(host == "recovery.cdt.ca.gov" for host in settings.ALLOWED_HOSTS):
         env = RUNTIME_ENVS.PROD
     return env
 
