6c956c2c017957f724a02e2c30bfeadda91ccd76
Machiko Yasuda <machiko@compiler.la>
2025-06-27 05:26:53 +0000

    feat(order): add regex for zipcode to allow 5 OR 9 with hyphen only

--- web/vital_records/forms.py
+++ web/vital_records/forms.py
@@ -219,8 +219,7 @@ class OrderInfoForm(forms.ModelForm):
             attrs={
                 "class": "form-control",
                 "inputmode": "numeric",
-                "minlength": "5",
-                "maxlength": "10",
+                "pattern": "[\d]{5}(-[\d]{4})?",
             }
         ),
     )
