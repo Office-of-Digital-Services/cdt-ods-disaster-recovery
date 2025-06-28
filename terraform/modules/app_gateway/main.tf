3df8a84012cfce57ebd930f56d8f00c7f61dabfa
Kegan Maher <kegan@compiler.la>
2025-06-27 17:08:29 -0700

    fix(tf/app_gateway): host_name needs to match the app host
    otherwise the gateway can't find a backend target for the traffic

--- terraform/modules/app_gateway/main.tf
+++ terraform/modules/app_gateway/main.tf
@@ -116,9 +116,7 @@ resource "azurerm_application_gateway" "main" {
     cookie_based_affinity               = "Disabled"
     # Always override the host header.
     pick_host_name_from_backend_address = false
-    # In prod, use the custom hostname.
-    # In non-prod, use the App Gateway's default FQDN.
-    host_name                           = var.is_prod ? var.hostname : azurerm_public_ip.app_gateway.fqdn
+    host_name                           = var.backend_fqdns.web
     path                                = "/"
     port                                = 80
     probe_name                          = "${local.probe_name}-web"
