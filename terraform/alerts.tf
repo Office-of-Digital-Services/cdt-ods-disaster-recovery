5e9d4b6b39d5810a8b725f98ee2dd35bac48e372
Kegan Maher <kegan@compiler.la>
2025-07-29 08:45:06 -0700

    feat(tf): alert on exceptions/errors
    query Application Insights every 5 minutes, looking at the exceptions
    and traces (with severity >= 3, "ERROR" and "CRITICAL") tables
    
    trigger an alert to the primary monitoring action group when count > 0

---
+++ terraform/alerts.tf
@@ -0,0 +1,41 @@
+locals {
+  app_error_subject = local.is_prod ? "🚨 Application Error" : "${local.env_name}: Application Error"
+  app_error_name    = "msqalert-cdt-pub-vip-ddrc-${local.env_letter}-001"
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "app_error" {
+  name                = local.app_error_name
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  scopes              = [module.monitoring.application_insights_id]
+
+  description  = "Alerts when any exception is logged in Application Insights."
+  display_name = local.app_error_name
+  enabled      = true
+  severity     = 2
+
+  # 5 minutes, in ISO 8601 duration format
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+
+  criteria {
+    query     = <<-QUERY
+      union exceptions, (traces | where severityLevel >= 3)
+    QUERY
+    operator  = "GreaterThan"
+    threshold = 0
+    time_aggregation_method = "Count"
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  action {
+    action_groups = [module.monitoring.action_group_id]
+    custom_properties = {
+      subject = local.app_error_subject
+    }
+  }
+}
