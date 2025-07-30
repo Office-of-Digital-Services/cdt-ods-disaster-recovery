5d5394e554ce34f7e003dd0732b63cd62143b0a1
Kegan Maher <kegan@compiler.la>
2025-07-30 00:16:54 +0000

    feat(urls): debug endpoints to test instrumentation

--- web/urls.py
+++ web/urls.py
@@ -5,10 +5,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/topics/http/urls/
 """
 
+import logging
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 from web.vital_records.hooks import VitalRecordsHooks
+
+logger = logging.getLogger(__name__)
 
 # /
 urlpatterns = [
@@ -18,3 +24,16 @@ urlpatterns = [
     path("oauth/", include("cdt_identity.urls"), {"hooks": VitalRecordsHooks}),
     path("vital-records/", include("web.vital_records.urls")),
 ]
+
+# register view handlers for debugging
+if settings.DEBUG:
+
+    def debug_error(request):
+        logger.error("Debug error")
+        return HttpResponse("Debug error was logged")
+
+    def debug_exception(request):
+        raise RuntimeError("Debug exception")
+
+    urlpatterns.append(path("error", debug_error))
+    urlpatterns.append(path("exception", debug_exception))
