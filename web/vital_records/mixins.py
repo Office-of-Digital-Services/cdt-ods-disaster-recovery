af7bc563d20150a53d54f97e2e2636d6f479ac65
Angela Tran <angela@compiler.la>
2025-08-15 22:21:27 +0000

    feat: introduce mixin that provides context about steps

--- web/vital_records/mixins.py
+++ web/vital_records/mixins.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseForbidden
+from django.urls import reverse
 
+from web.vital_records.routes import Routes
 from web.vital_records.session import Session
 
 
@@ -12,3 +14,49 @@ class ValidateRequestIdMixin:
             return super().dispatch(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
+
+
+class StepsContextMixin:
+    STEPS = {
+        "birth": {
+            "Name": Routes.birth_request_name,
+            "County of birth": Routes.birth_request_county,
+            "Date of birth": Routes.birth_request_dob,
+            "Parents' names": Routes.birth_request_parents,
+            "Order information": Routes.request_order,
+            "Preview & submit": Routes.request_submit,
+        },
+        "marriage": {
+            "Name": Routes.marriage_request_name,
+            "County of marriage": Routes.marriage_request_county,
+            "Date of marriage": Routes.marriage_request_date,
+            "Order information": Routes.request_order,
+            "Preview & submit": Routes.request_submit,
+        },
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        type_steps = self.STEPS[self.object.type]
+        step_names = type_steps.keys()
+        current_index = step_names.index(self.step_name)
+
+        context["all_steps"] = step_names
+        context["step_number"] = current_index + 1
+
+        if current_index == 0:
+            context["previous_route"] = Routes.request_statement
+        else:
+            previous_step_name = step_names[current_index - 1]
+            context["previous_route"] = type_steps[previous_step_name]
+
+        if current_index == len(step_names) - 1:
+            next_route = Routes.request_submitted
+        else:
+            next_step_name = step_names[current_index + 1]
+            next_route = type_steps[next_step_name]
+
+        self.success_url = reverse(next_route, kwargs={"pk": self.object.pk})
+
+        return context
