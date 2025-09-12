6d7be172130ebc3b9e8508fa56560f8586b24230
Luis Alvergue <luis@compiler.la>
2025-09-12 16:38:59 +0000

    refactor(models): move STATE_CHOICES out of VitalRecordsRequest
    associated with state field

--- web/vital_records/models.py
+++ web/vital_records/models.py
@@ -20,69 +20,6 @@ class VitalRecordsRequest(models.Model):
 
     NUMBER_CHOICES = [(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5"), (6, "6"), (7, "7"), (8, "8"), (9, "9"), (10, "10")]
 
-    STATE_CHOICES = [
-        ("", "Select state"),
-        ("AK", "Alaska"),
-        ("AL", "Alabama"),
-        ("AR", "Arkansas"),
-        ("AS", "American Samoa"),
-        ("AZ", "Arizona"),
-        ("CA", "California"),
-        ("CO", "Colorado"),
-        ("CT", "Connecticut"),
-        ("DC", "District of Columbia"),
-        ("DE", "Delaware"),
-        ("FL", "Florida"),
-        ("FM", "Federated States of Micronesia"),
-        ("GA", "Georgia"),
-        ("GU", "Guam"),
-        ("HI", "Hawaii"),
-        ("IA", "Iowa"),
-        ("ID", "Idaho"),
-        ("IL", "Illinois"),
-        ("IN", "Indiana"),
-        ("KS", "Kansas"),
-        ("KY", "Kentucky"),
-        ("LA", "Louisiana"),
-        ("MA", "Massachusetts"),
-        ("MD", "Maryland"),
-        ("ME", "Maine"),
-        ("MH", "Marshall Islands"),
-        ("MI", "Michigan"),
-        ("MN", "Minnesota"),
-        ("MO", "Missouri"),
-        ("MP", "Northern Mariana Islands"),
-        ("MS", "Mississippi"),
-        ("MT", "Montana"),
-        ("NC", "North Carolina"),
-        ("ND", "North Dakota"),
-        ("NE", "Nebraska"),
-        ("NH", "New Hampshire"),
-        ("NJ", "New Jersey"),
-        ("NM", "New Mexico"),
-        ("NV", "Nevada"),
-        ("NY", "New York"),
-        ("OH", "Ohio"),
-        ("OK", "Oklahoma"),
-        ("OR", "Oregon"),
-        ("PA", "Pennsylvania"),
-        ("PR", "Puerto Rico"),
-        ("PW", "Palau"),
-        ("RI", "Rhode Island"),
-        ("SC", "South Carolina"),
-        ("SD", "South Dakota"),
-        ("TN", "Tennessee"),
-        ("TX", "Texas"),
-        ("UT", "Utah"),
-        ("VA", "Virginia"),
-        ("VI", "Virgin Islands"),
-        ("VT", "Vermont"),
-        ("WA", "Washington"),
-        ("WI", "Wisconsin"),
-        ("WV", "West Virginia"),
-        ("WY", "Wyoming"),
-    ]
-
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     status = FSMField(default="initialized", choices=STATUS_CHOICES)
     type = models.CharField(max_length=10)
@@ -108,7 +45,7 @@ class VitalRecordsRequest(models.Model):
     address = models.CharField(max_length=128)
     address_2 = models.CharField(max_length=128)
     city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2, choices=STATE_CHOICES)
+    state = models.CharField(max_length=2)
     zip_code = models.CharField(max_length=10)
     email_address = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=10)
