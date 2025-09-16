d6a71357e6493c19e85c9dfe51938c1184c1a0c3
Luis Alvergue <luis@compiler.la>
2025-09-16 15:18:56 +0000

    feat: add Name form and view for death flow

--- web/vital_records/views/death.py
+++ web/vital_records/views/death.py
@@ -1,10 +1,27 @@
 from django.views.generic.edit import UpdateView
 
+from web.vital_records.forms.death import NameForm
+from web.vital_records.mixins import ValidateTypeMixin
 from web.vital_records.views import common
 
 
-class NameView(common.NameView):
-    pass
+class NameView(ValidateTypeMixin, common.NameView):
+    form_class = NameForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_question"] = "What is the name on the death record?"
+        context["form_hint"] = "Please write the information as it appears on the death record."
+        context["form_hint_name"] = "name-hint"
+        form = context["form"]
+
+        context["form_fields"] = [
+            form["first_name"],
+            form["middle_name"],
+            form["last_name"],
+        ]
+
+        return context
 
 
 class CountyView(common.CountyView):
